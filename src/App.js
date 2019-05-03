408cfe02e006e172a33d07425bb23a178db1f2de
gasseklopper <m-morley@gmx.de>
2019-05-03 11:30:07 +0000

    added jsx render to show username with favorite movie

--- src/App.js
+++ src/App.js
@@ -105,7 +105,19 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">ReactND - Coding Practice</h1>
         </header>
-        <h2>Favorite Movies</h2>
+		<ul>
+          <h2>Favorite Movies</h2>
+          {profiles.map(profile => {
+           		const userName = users[profile.userID].name
+				const favMovieName = movies[profile.favoriteMovieID].name
+				
+				return (
+                  <li key={profile.id}>
+					<p>{`${userName}\'s favorite movie is "${favMovieName}."`}</p>
+				</li>
+                )
+		})}
+		</ul>
       </div>
     );
   }
